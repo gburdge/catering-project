e3b4936ccfba1d163b17c203ccb54894e0e39777
Greg Burdge <codingcorner@outlook.com>
2014-07-09 13:52:15 -0600

    added co, menu and food items models

--- catering-backend/apps/public/models.py
+++ catering-backend/apps/public/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here, i.e.: #photo, ingredients, description, instructions, prep time, total time, tags, favorited
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone = models.IntegerField(max_length=9)
+    cuisine = models.ForeignKey("Cuisine")
+    menu = models.OneToOneField("Menu", null=True, blank=True)
+    image = models.CharField(max_length=500, default="no image")
+
+    def __unicode__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+
+class FoodItems(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.TextField()
+    price = models.IntegerField(max_length=9)
+    image = models.CharField(max_length=500, default="no image")
+
+
+class Cuisine(models.Model):
+    name = models.CharField(max_length=50)
+
