a4d82c8cc0069bf3d8087fcef6d97faf44460cb0
Noe Rodriguez <n@n.com>
2014-07-09 10:17:41 -0600

    testing

---
+++ catering-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cateringFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cateringFrontendApp
+ */
+angular.module('cateringFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
