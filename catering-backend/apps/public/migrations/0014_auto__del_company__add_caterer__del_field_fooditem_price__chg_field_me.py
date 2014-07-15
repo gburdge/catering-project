648e02e22c62185341ea14bed710c661c1c138fc
Greg Burdge <codingcorner@outlook.com>
2014-07-15 10:33:37 -0600

    switched out databases

--- catering-backend/apps/public/migrations/0014_auto__del_company__add_caterer__del_field_fooditem_price__chg_field_me.py
+++ catering-backend/apps/public/migrations/0014_auto__del_company__add_caterer__del_field_fooditem_price__chg_field_me.py
@@ -8,17 +8,40 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Deleting model 'Company'
+        db.delete_table(u'public_company')
+
+        # Adding model 'Caterer'
+        db.create_table(u'public_caterer', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('address', self.gf('django.db.models.fields.CharField')(max_length=60, null=True, blank=True)),
+            ('phone', self.gf('django.db.models.fields.IntegerField')(max_length=10, null=True, blank=True)),
+            ('menu', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['public.Menu'], null=True, blank=True)),
+        ))
+        db.send_create_signal(u'public', ['Caterer'])
+
         # Deleting field 'FoodItem.price'
         db.delete_column(u'public_fooditem', 'price')
 
-
-        # Changing field 'Company.menu'
-        db.alter_column(u'public_company', 'menu_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['public.Menu'], null=True))
 
         # Changing field 'Menu.FoodItem'
         db.alter_column(u'public_menu', 'FoodItem_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['public.FoodItem'], null=True))
 
     def backwards(self, orm):
+        # Adding model 'Company'
+        db.create_table(u'public_company', (
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('menu', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['public.Menu'])),
+            ('phone', self.gf('django.db.models.fields.IntegerField')(max_length=10)),
+            ('address', self.gf('django.db.models.fields.CharField')(max_length=60)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        ))
+        db.send_create_signal(u'public', ['Company'])
+
+        # Deleting model 'Caterer'
+        db.delete_table(u'public_caterer')
+
 
         # User chose to not deal with backwards NULL issues for 'FoodItem.price'
         raise RuntimeError("Cannot reverse this migration. 'FoodItem.price' and its values cannot be restored.")
@@ -29,13 +52,6 @@ class Migration(SchemaMigration):
                       keep_default=False)
 
 
-        # User chose to not deal with backwards NULL issues for 'Company.menu'
-        raise RuntimeError("Cannot reverse this migration. 'Company.menu' and its values cannot be restored.")
-        
-        # The following code is provided here to aid in writing a correct migration
-        # Changing field 'Company.menu'
-        db.alter_column(u'public_company', 'menu_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['public.Menu']))
-
         # User chose to not deal with backwards NULL issues for 'Menu.FoodItem'
         raise RuntimeError("Cannot reverse this migration. 'Menu.FoodItem' and its values cannot be restored.")
         
@@ -44,13 +60,13 @@ class Migration(SchemaMigration):
         db.alter_column(u'public_menu', 'FoodItem_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['public.FoodItem']))
 
     models = {
-        u'public.company': {
-            'Meta': {'object_name': 'Company'},
-            'address': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
+        u'public.caterer': {
+            'Meta': {'object_name': 'Caterer'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '60', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'menu': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['public.Menu']", 'null': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'phone': ('django.db.models.fields.IntegerField', [], {'max_length': '10'})
+            'phone': ('django.db.models.fields.IntegerField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'})
         },
         u'public.fooditem': {
             'Meta': {'object_name': 'FoodItem'},
