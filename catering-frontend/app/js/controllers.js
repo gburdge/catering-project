e838c7b7acf7ba9feb852423ba51113ac465cd70
Noe Rodriguez <n@n.com>
2014-07-17 11:28:54 -0600

    added the to order table for each order

--- catering-frontend/app/js/controllers.js
+++ catering-frontend/app/js/controllers.js
@@ -22,14 +22,37 @@ angular.module('myApp.controllers', [])
         $scope.increment_Qty= function(food_item) {
 
            food_item.quantity += 1;
+           $scope.addToOrder(food_item);
 
         }
+
+        $scope.order = {
+            totalCost: 0.00,
+            items: [],
+            itemCount: 0
+        }
+        $scope.addToOrder = function(foodItem) {
+            if (!($scope.order.items.indexOf(foodItem) > -1)) {
+                $scope.order.items.push(foodItem);
+            }
+            $scope.order.totalCost += parseFloat(foodItem.price);
+            $scope.order.itemCount += 1;
+        }
+
+        $scope.removeFromOrder = function(foodItem) {
+            // To finish this function, implement bounds checking.
+            // Cannot have negative quantities, and remove from the array once you get to 0.
+
+            $scope.order.totalCost -= parseFloat(foodItem.price);
+            $scope.order.itemCount -= 1;
+        }
+
         $scope.decrease_Qty= function(food_item){
 
             if (food_item.quantity > 0){
                     food_item.quantity -= 1;
             }
-
+            $scope.removeFromOrder(food_item);
         }
 
         $scope.calculatePrice= function(food_item){
@@ -37,7 +60,6 @@ angular.module('myApp.controllers', [])
             var price =  food_item.price * food_item.quantity;
 
             return "$"+price.toFixed(2);
-
         }
 
     })
